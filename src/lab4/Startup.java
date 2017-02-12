6ad7d05c4dbfa23c12f8069008882a6d736aa503
roshann <roshann@10.30.23.52>
2017-02-12 01:18:54 -0600

    implemented startup method

--- src/lab4/Startup.java
+++ src/lab4/Startup.java
@@ -1,5 +1,7 @@
 package lab4;
 
+import java.util.Scanner;
+
 /**
  * This class is the startup class for the program. But together with the 
  * other classes provided it is not a particularly good simulation of the 
@@ -13,11 +15,38 @@ public class Startup {
     public static void main(String[] args) {
         
         Company abc = new Company();
-        abc.hireEmployee("Binuth", "Nanayakkara", "643-23-3070", "A102");
-
-//        Employee employee = new Employee("Peter","Piper","333-33-3333");
-//        
-//        employee.doFirstTimeOrientation("A101");
-//        employee.getReportService().outputReport();
+         //abc.hireEmployee("Binuth", "Nanayakkara", "643-23-3070");
+        Scanner input = new Scanner(System.in);
+        String ans = "";
+        do {
+            System.out.print("Enter employee first name : ");
+            String fName = input.next();
+            System.out.print("Enter employee last name : ");
+            String lName = input.next();           
+            System.out.print("Enter employee ssn : ");
+            String ssn = input.next();
+            abc.hireEmployee(fName, lName, ssn);
+            
+            System.out.print("Do you want to hire more employee (Y/N) : ");
+            ans = input.next();
+            
+        }while(!"N".equalsIgnoreCase(ans));
+        
+        ans = "";
+        int index = 0;
+        do{
+            if (abc.getHireResources().getEmployee().size() > index){
+                String cube = abc.getHireResources().getEmployee().get(index).getCubeId();
+                abc.getHireResources().getEmployee().get(index).doFirstTimeOrientation(cube);
+                abc.getHireResources().getEmployee().get(index).getReportService().outputReport();
+            }else {
+                System.out.println("No more employes to send orientation ");
+            }
+            //abc.getHireResources().getEmployee().get(index).getReportService();
+            index++;
+            System.out.print("Send more employees to orientation (Y/N) : ");
+            ans = input.next();
+        } while (!"N".equalsIgnoreCase(ans));
+        
     }    
 }
